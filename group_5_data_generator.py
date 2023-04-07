ee124865f7a7b5b3d9ee271e4dac0b66f15493ec
nguye <vinh.nguyenhuynhquang@gmail.com>
2023-04-07 10:27:39 -0400

    git init

--- group_5_data_generator.py
+++ group_5_data_generator.py
@@ -10,12 +10,16 @@ class Util:
     y_data=[];
     def __init__(self):
         self.start_id = 0
+        self.temp = 0
+        self.diff_temp = "-"
 
     def get_json_data(self):
         self.start_id += 1
         x = random.random()
-        temp = round(10*x+15,2)
+        new_temp = round(10*x+15,2)
+        if self.start_id>0: self.diff_temp= round(new_temp - self.temp,2)
+        self.temp = new_temp
         humid = round(0.4+x/10,2)
         aqi = round(25+x*10,0)
-        data = {'id': self.start_id, 'time': asctime(), 'temp': temp, 'humid': humid, 'AQI': aqi}
+        data = {'id': self.start_id, 'time': asctime(), 'temp': self.temp, 'humid': humid, 'AQI': aqi, "diff_temp": self.diff_temp}
         return dumps(data, indent=2)
